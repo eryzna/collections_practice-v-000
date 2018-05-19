bad54a9f80076a9a30cb90d36ba26369909b0f7d
Elana Ryznar <eryzna@gmail.com>
2018-05-19 21:36:31 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -42,5 +42,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect {|element, index| element[0] +"s"}
+  array.each_with_index.collect {|element, index| element[0] "s"}
 end
